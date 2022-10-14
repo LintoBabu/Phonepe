39bb1c01a8f60225ca115265a837aa3286af6331
LINTO <lintob4@gmail.com>
2022-10-14 10:36:19 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class phonepe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: loginPage()
     );
   }
